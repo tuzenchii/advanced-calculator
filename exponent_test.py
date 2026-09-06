54ebedb486f42da79f8736f1be7e564e4a6dcc77
tuzenchii <trevonmdennis@proton.me>
2026-09-06 14:56:04 -0500

    Create exponent_test.py
    random file for the future

---
+++ exponent_test.py
@@ -0,0 +1,7 @@
+def exponent(base, exponent):
+    return base ** exponent
+
+base = float(input("Enter the base number: "))
+exponent_value = float(input("Enter the exponent: "))
+result = exponent(base, exponent_value)
+print(f"{base:,g} raised to the power of {exponent_value:,g} is: {result:,g}")
